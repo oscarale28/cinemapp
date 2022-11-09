bc070cc950e952a0d1f69476f3d9f566df567bc4
Oscar Orellana <oscarore284@gmail.com>
2022-11-09 12:01:33 -0600

    Maquetado carousel de géneros

--- js/index.js
+++ js/index.js
@@ -15,3 +15,33 @@ const removeActiveClass = () => {
         panel.classList.remove("active");
     });
 };
+
+var owl = $('.owl-carousel');
+
+owl.owlCarousel({
+    loop:true,
+    margin:10,
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+        1280:{
+            items:3
+        },
+        1920 : {
+            items: 5
+        }
+    }
+});
+
+owl.on('mousewheel', '.owl-stage', function (e) {
+    if (e.deltaY>0) {
+        owl.trigger('next.owl');
+    } else {
+        owl.trigger('prev.owl');
+    }
+    e.preventDefault();
+});
